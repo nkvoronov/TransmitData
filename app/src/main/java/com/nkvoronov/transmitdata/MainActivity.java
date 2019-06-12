04aff57de309fae781d18cccb96edc1f248064d7
Nikolay Voronov <nkvoronov@gmail.com>
2019-06-12 15:47:21 +0300

    Initial commit

---
+++ app/src/main/java/com/nkvoronov/transmitdata/MainActivity.java
@@ -0,0 +1,22 @@
+package com.nkvoronov.transmitdata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, TransmitActivity.class);
+        intent.putExtra("Ключ", "Значение");
+        startActivity(intent);
+    }
+}
